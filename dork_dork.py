6840201c3221609f3ff41c56b66155aee4d8d745
Safin Mohammad <151577677+Safin-Mohammad@users.noreply.github.com>
2025-12-12 22:11:13 +0600

    Add common search queries for dorking

---
+++ dork_dork.py
@@ -0,0 +1,7 @@
+site:.com inurl:.php?id=
+site:.ru inurl:russian_cv filetype:pdf
+intitle:password/email filetype:doc
+filetype:log inurl:access_log
+intitle:"Index of" .git
+inurl:web.config filetype:config
+intext:"# FTP connection parameters" filetype:txt
